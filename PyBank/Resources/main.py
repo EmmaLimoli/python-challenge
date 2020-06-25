43b34c51ed4e4226b925c1e1f2ede42d4f3cc547
Emma Limoli <EmmaLimoli024@gmail.com>
2020-06-25 12:22:32 -0400

    got total months

--- PyBank/Resources/main.py
+++ PyBank/Resources/main.py
@@ -7,60 +7,56 @@ pybank_csv = os.path.join("..", "Resources", "budget_data.csv")
 
 #define function and use pybank_data as parameter
 #def financial_analysis(record_data):
+with open(pybank_csv, newline='') as csvfile:
+    c = csv.reader(csvfile, delimiter=",")
+    headers = next(c, None)
+    data_group = input("Press Enter for financial analysis breakdown")
+    some_var = 0
 
-#define variables 
-    #time = int(record_data[0])
-    #profit_loss = int(record_data[1])
-    #total = 0
+    #total months 
+    profit_loss = []
+    row = [0]
+    total_months = str(row[0])
+    rows = len(total_months)
+    revenue = []
+    date = []
+    rev_change = []
+    #total average of profit/loss
+   
+
+    #loop through data
+    for row in c:
+        revenue.append(float(row[1]))
+        date.append(row[0])
+        
+
+        #average = int(row[1])
+        #if average > 0:
+            #print(average)
+        #elif average < 0:
+            #print(average)
+
     
-
-#define equations
-
-
-    #net_amount =
-    #average_change = 
-    #increase = 
-    #decrease =
-
-#if/else statements 
-#pull total number of months in data
-#pull net total amount of profit/loss over entire period
-#pull average of the changes in profit/loss
-#greatest increase in profits(date and amount) over entire period
-#greatest decrease in losses (date and amount) over entire period
-#print statements
-#with open
-with open(pybank_csv, 'r') as csvfile:
-        c = csv.reader(csvfile, delimiter=",")
-        headers = next(c, None)
-        data_group = input("Press Enter for financial analysis breakdown")
-        some_var = 0
-        some_list = []
-        for r in c:
-            some_var += int(float(r[1]))
-            some_list.append(int(float(r[1])))
-some_var
-sum(some_list)
-print(f'Total: ${sum(some_list)}')
-
-
-
-        
-        
-        
-        
-        
-        #csvreader = csv.reader(csvfile, delimiter=",")
-        #header = next(csvreader)
-        #
-
-        #for row in csvreader:
-            #if data_group == row[0]:
-                #print(row[0])
-
-
-
-
+ 
+        #total profit data
+        some_var += int(float(row[1]))
+        profit_loss.append(int(float(row[1])))
             
+some_var
+sum(profit_loss)
+#sum(total_months)
+print(f'Total Months: {len(date)}')
+print(f'Total: ${sum(profit_loss)}')
+print("Greatest Increase In Profits:", len(date))
+print("Total Revenue: $", sum(revenue))
 
-#save as txt file
+#print greatest increase in profits
+#print greatest decrease in profits
+
+
+
+print 
+
+
+
+
